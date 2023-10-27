bae386257fa0796ca04cf3fd323d29e729e430d7
Arijit-Kumar-Pandab <arijitkumarpandab@gmail.com>
2023-10-27 21:15:34 +0530

    First Commit

---
+++ Assignment-4/src/assignments/ineuron/RemoveDuplicatesFromString.java
@@ -0,0 +1,22 @@
+package assignments.ineuron;
+
+public class RemoveDuplicatesFromString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "sinstriiintng";
+		System.out.println(removeDuplicates(str));
+	}
+	private static String removeDuplicates(String str) {
+		StringBuilder res = new StringBuilder("");
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (res.indexOf(ch + "") == -1) {
+				res.append(ch);
+			}
+		}
+		
+		return res.toString();
+	}
+
+}
